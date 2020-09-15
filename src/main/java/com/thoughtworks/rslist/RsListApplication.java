429e4f3bee39e34a8c6bdf4c54735c5462e31617
xiao luo <xiao.luo@thoughtworks.com>
2020-09-15 22:28:01 +0800

    [luoxiao] Complete the exercise

--- src/main/java/com/thoughtworks/rslist/RsListApplication.java
+++ src/main/java/com/thoughtworks/rslist/RsListApplication.java
@@ -3,6 +3,8 @@ package com.thoughtworks.rslist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class RsListApplication {
 
--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,42 @@
 package com.thoughtworks.rslist.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.thoughtworks.rslist.domain.RsEvent;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<RsEvent> rsList = initRsEventList();
+
+    private List<RsEvent> initRsEventList() {
+        List<RsEvent> rsEventsList = new ArrayList<>();
+        rsEventsList.add(new RsEvent("第一条事件","无标签"));
+        rsEventsList.add(new RsEvent("第二条事件","无标签"));
+        rsEventsList.add(new RsEvent("第三条事件","无标签"));
+        return rsEventsList;
+    }
+
+
+    /* @GetMapping("/rs/list") public String getRsList(){
+       return rsList.toString();
+     }*/
+  @GetMapping("/rs/{index}")
+  public RsEvent getOneRsEvent(@PathVariable int index){
+      return rsList.get(index - 1);
+  }
+  @GetMapping("/rs/list")
+  public List<RsEvent> getRsEventBetween(@RequestParam(required = false) Integer start,@RequestParam(required = false) Integer end){
+    if(start == null || end == null){
+      return rsList;
+    }
+    return rsList.subList(start-1,end);
+  }
+
+  @PostMapping("/rs/event") public void addRsEvent(@RequestBody RsEvent rsEvent){
+      rsList.add(rsEvent);
+  }
 }
+
